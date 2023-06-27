b7453507680364d8f36ab8b26c60d097233a7bef
daniel.huaman.contractor <dhuaman9@outlook.com>
2023-06-27 16:52:32 -0500

    first commit

---
+++ src/app/_shared/var.constant.ts
@@ -0,0 +1,5 @@
+
+export const HOST = 'http://192.168.1.8:10402/ms365/api';
+export const HOSTLOGIN = 'http://192.168.1.8:10402/ms365';
+export const TOKEN_NAME = 'access_token';
+export const TOKEN_ROLE = 'role';
